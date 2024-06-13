68b5969fcdfdf3aa054af14d25a24edd49dd6f8a
amimbs <andymimbs91@gmail.com>
2024-06-13 01:18:52 -0400

    bug: bind this to countdowntime

--- src/app/features/timer/timer.component.ts
+++ src/app/features/timer/timer.component.ts
@@ -5,9 +5,9 @@ import { Component, OnInit } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './timer.component.html',
-  styleUrl: './timer.component.scss'
+  styleUrls: ['./timer.component.scss']
 })
-export class TimerComponent implements OnInit{
+export class TimerComponent implements OnInit {
   semiCircles: any = [];
   hr: number = 0;
   min: number = 0;
@@ -20,44 +20,36 @@ export class TimerComponent implements OnInit{
   setTime: number = this.hours + this.minutes + this.seconds;
   startTime = Date.now();
   futureTime = this.startTime + this.setTime;
-  timerLoop = setInterval(this.countDownTimer);
+  timerLoop: any;
   currentTime: number = Date.now();
   remainingTime?: number;
   angle?: any;
 
   ngOnInit(): void {
-    this.semiCircles = document.querySelectorAll('.semiCircle')
-
-    this.countDownTimer()
-
-
-       console.log(this.semiCircles, "hi")
+    this.semiCircles = document.querySelectorAll('.semiCircle');
+    this.countDownTimer = this.countDownTimer.bind(this); // Bind the context
+    this.timerLoop = setInterval(this.countDownTimer); // Set interval for 1 second
+    this.countDownTimer();
   }
 
-
-
   countDownTimer() {
+    this.currentTime = Date.now();
     this.remainingTime = this.futureTime - this.currentTime;
     this.angle = (this.remainingTime / this.setTime) * 360;
 
     // progress indicator
-    if(this.angle > 180) {
+    if (this.angle > 180) {
       this.semiCircles[2].style.display = 'none';
       this.semiCircles[0].style.transform = 'rotate(180deg)';
-      this.semiCircles[1].style.transform = `rotate(${this.angle}deg`;
+      this.semiCircles[1].style.transform = `rotate(${this.angle - 180}deg)`;
     } else {
       this.semiCircles[2].style.display = 'block';
       this.semiCircles[0].style.transform = `rotate(${this.angle}deg)`;
-      this.semiCircles[1].style.transform = `rotate(${this.angle}deg`;
+      this.semiCircles[1].style.transform = `rotate(${this.angle}deg)`;
     }
 
     // timer
-
     // 5 sec conditional
-
     // end
   }
-
 }
-
-//left off, timerLoop is populating with undefined values

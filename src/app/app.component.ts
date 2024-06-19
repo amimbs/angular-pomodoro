0ed8b3b17f676c89697969ce8941688d61e02003
amimbs <andymimbs91@gmail.com>
2024-06-19 02:59:55 -0400

    feat: add clock component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TimerComponent } from "./features/timer/timer.component";
+import { TimerClockComponent } from './features/timer-clock/timer-clock.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, TimerComponent]
+    imports: [RouterOutlet, TimerComponent, TimerClockComponent]
 })
 export class AppComponent {
   title = 'pomodoro-app';
